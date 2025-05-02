8c36823066f820aacca185f3c233a01021a1ba2c
horadev <horacio@megawebs.com.ar>
2025-05-02 19:33:56 -0300

    initial commit

---
+++ src/test/java/ar/edu/itba/demo/UsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.itba.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
