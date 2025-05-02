3ebab0ecf41c96e2b01243bd00e7f8a1fa793cab
horadev <horacio@megawebs.com.ar>
2025-05-02 19:59:10 -0300

    feature/open api spring-doc

---
+++ src/main/java/ar/edu/itba/demo/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package ar.edu.itba.demo.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Demo ITBA Usuarios API")
+                        .version("0.0.1-SNAPSHOT")
+                        .description("API para CRUD de Usuarios"));
+    }
+}
