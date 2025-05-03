6fac77b06e9b98550182a9a9d1349d4deb0a357d
horadev <horacio@megawebs.com.ar>
2025-05-02 21:47:01 -0300

    feature/h2, initialize & load db & model entity usuarios

---
+++ src/main/java/ar/edu/itba/demo/config/DBInitializer.java
@@ -0,0 +1,49 @@
+package ar.edu.itba.demo.config;
+
+import ar.edu.itba.demo.model.Usuario;
+import ar.edu.itba.demo.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
+
+@Configuration
+@RequiredArgsConstructor
+public class DBInitializer implements CommandLineRunner{
+
+    private final JdbcTemplate jdbcTemplate;
+    private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+    @Override
+    public void run(String... args) throws Exception {
+
+        jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS usuarios (id int primary key "
+                + "auto_increment, nombre varchar(30), apellido varchar(30), dni varchar(10), email varchar(35), " +
+                "username varchar(30), password varchar(30))");
+
+
+        jdbcTemplate.execute("""
+                INSERT INTO usuarios (nombre, apellido, dni, email, username, password)
+                VALUES ('Juan', 'Pérez', '28456789A', 'juan.perez@email.com', 'jperez', 'Clave1234');""");
+
+        jdbcTemplate.execute("""
+                INSERT INTO usuarios (nombre, apellido, dni, email, username, password)
+                VALUES ('María', 'Rodríguez', '35782914B', 'maria.rodriguez@email.com', 'mrodriguez', 'Maria2025');""");
+
+        jdbcTemplate.execute("""
+                INSERT INTO usuarios (nombre, apellido, dni, email, username, password)
+                VALUES ('Carlos', 'González', '42198367C', 'carlos.gonzalez@email.com', 'cgonzalez', 'Segura456');""");
+
+        Usuario usuario = Usuario.builder()
+                .nombre("Luis")
+                .apellido("Fernandez")
+                .dni("12345678D")
+                .email("luis.fernandez@email.com")
+                .username("lfernandez")
+                .password("Luis1234")
+                    .build();
+        usuarioRepository.save(usuario);
+    }
+}
---
+++ src/main/java/ar/edu/itba/demo/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package ar.edu.itba.demo.repository;
+
+import ar.edu.itba.demo.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
