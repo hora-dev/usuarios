3d59aa149cd9f1848823c594efd14952bf6e71f2
hora-dev <horacio@megawebs.com.ar>
2025-05-03 20:30:22 -0300

    feature/tests & leer_usuario_por_id & leer todos usuarios (#8)

--- src/test/java/ar/edu/itba/demo/service/UsuarioServiceTest.java
+++ src/test/java/ar/edu/itba/demo/service/UsuarioServiceTest.java
@@ -10,11 +10,12 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
@@ -53,7 +54,7 @@ class UsuarioServiceTest {
     }
 
     @Test
-    void testNuevoUsuario() throws ExecutionException, InterruptedException {
+    void testNuevoUsuarioOk() throws ExecutionException, InterruptedException {
         when( usuarioRepository.save( Mockito.any( Usuario.class ) ) ).thenReturn( usuario );
 
         CompletableFuture<Usuario> usuarioGuardadoFuturo = usuarioService.crearUsuario( usuarioDTO );
@@ -67,4 +68,50 @@ class UsuarioServiceTest {
         assertEquals( usuarioGuardadoFuturo.get().getPassword(), usuarioDTO.getPassword() );
         verify( usuarioRepository, times(1) ).save( Mockito.any( Usuario.class ) );
     }
+
+    @Test
+    void testObtenerUsuarioOk() {
+
+        when( usuarioRepository.findById(1L) ).thenReturn( Optional.of(usuario) );
+
+        Optional<Usuario> usuarioPorId = usuarioService.obtenerUsuario(1L);
+
+        assertTrue( usuarioPorId.isPresent() );
+        assertEquals( usuarioPorId.get().getApellido(), usuario.getApellido() );
+        assertEquals( usuarioPorId.get().getNombre(), usuario.getNombre() );
+        assertEquals( usuarioPorId.get().getDni(), usuario.getDni() );
+        assertEquals( usuarioPorId.get().getEmail(), usuario.getEmail() );
+        assertEquals( usuarioPorId.get().getUsername(), usuario.getUsername() );
+        assertEquals( usuarioPorId.get().getPassword(), usuario.getPassword() );
+        verify( usuarioRepository, times(1) ).findById(1L   );
+    }
+
+    @Test
+    void testObtenerUsuarioNoEncontrado() {
+
+        when( usuarioRepository.findById(1L) ).thenReturn( Optional.empty() );
+
+        Optional<Usuario> usuarioPorId = usuarioService.obtenerUsuario(1L);
+
+        assertFalse( usuarioPorId.isPresent() );
+        verify( usuarioRepository, times(1) ).findById(1L   );
+    }
+    
+    @Test
+    void testObtenerTodosLosUsuarios() {
+
+        when( usuarioRepository.findAll() ).thenReturn( List.of( usuario ) );
+
+        List<Usuario> todosLosUsuarios = usuarioService.obtenerTodosLosUsuarios();
+        
+        assertFalse( todosLosUsuarios.isEmpty() );
+        assertEquals( todosLosUsuarios.getFirst().getApellido(), usuario.getApellido() );
+        assertEquals( todosLosUsuarios.getFirst().getNombre(), usuario.getNombre() );
+        assertEquals( todosLosUsuarios.getFirst().getDni(), usuario.getDni() );
+        assertEquals( todosLosUsuarios.getFirst().getEmail(), usuario.getEmail() );
+        assertEquals( todosLosUsuarios.getFirst().getUsername(), usuario.getUsername() );
+        assertEquals( todosLosUsuarios.getFirst().getPassword(), usuario.getPassword() );
+        verify( usuarioRepository, times(1) ).findAll();
+    }
+
 }
